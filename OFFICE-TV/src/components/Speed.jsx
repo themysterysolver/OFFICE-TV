bcd7169ecdf04e01af85a49d57287c300b5feaba
themysterysolver <arjunprabhakar1910@gmail.com>
2026-05-17 18:21:04 +0530

    FEAT:randomness+speed

--- OFFICE-TV/src/components/Speed.jsx
+++ OFFICE-TV/src/components/Speed.jsx
@@ -3,16 +3,17 @@ import React from 'react'
 const Speed = ({speed,setSpeed}) => {
   return (
     <div className='controlBar'>
+        <button onClick={()=>setSpeed((s)=>{
+        if(s<100) return s*10
+        return 100
+        })}>Inc</button>
       <button onClick={()=>setSpeed((s)=>
         {if(s>0.1) return s/10
             return 0.01
         })
 
-        }>Inc</button>
-      <button onClick={()=>setSpeed((s)=>{
-        if(s<100) return s*10
-        return 100
-        })}>Dec</button>
+        }>Dec</button>
+      
     </div>
   )
 }
--- OFFICE-TV/src/components/Tv.jsx
+++ OFFICE-TV/src/components/Tv.jsx
@@ -5,10 +5,35 @@ const Tv = ({speed}) => {
     let DVD = useRef(null)
     let [coordinate,setCoordinate] = useState([0,0]) 
     let TV = useRef(null);
-    
-    let dirL = useRef(10)
-    let dirT = useRef(10)
+    const randomSpeed = () =>{
+      return Math.floor(Math.random()*2)+3
+    }
 
+    let dirL = useRef(1)
+    let dirT = useRef(1)
+
+    let dL = useRef(randomSpeed())
+    let dR = useRef(randomSpeed())
+    useEffect(()=>{
+      const el = DVD.current;
+      const tvEl = TV.current;
+      
+      
+      let elPos = el.getBoundingClientRect();
+      let tvElPos = tvEl.getBoundingClientRect();
+
+      let l = elPos.left-tvElPos.left;
+      let t = elPos.top-tvElPos.top;
+      
+      let w =  tvEl.clientWidth-el.offsetWidth;
+      let h =  tvEl.clientHeight-el.offsetHeight;
+      setCoordinate(([x,y])=>{
+          return [Math.floor(Math.random()*w),Math.floor(Math.random()*h)] //[l,t]
+      })
+
+      el.style.left = `${l}px`;
+      el.style.top = `${t}px`;
+    },[])
     useEffect(()=>{
         const el = DVD.current;
         const tvEl = TV.current;
@@ -16,15 +41,18 @@ const Tv = ({speed}) => {
         
         let elPos = el.getBoundingClientRect();
         let tvElPos = tvEl.getBoundingClientRect();
+
         let l = elPos.left-tvElPos.left;
         let t = elPos.top-tvElPos.top;
         
-        setCoordinate(([x,y])=>{
-            return [l,t]
-        })
+        let w =  tvEl.clientWidth-el.offsetWidth;
+        let h =  tvEl.clientHeight-el.offsetHeight;
+        // setCoordinate(([x,y])=>{
+        //     return [Math.floor(Math.random()*w),Math.floor(Math.random()*h)] //[l,t]
+        // })
 
-        el.style.left = `${l}px`;
-        el.style.top = `${t}px`;
+        // el.style.left = `${l}px`;
+        // el.style.top = `${t}px`;
         
         let idx = 0;
         // let dir = [[10,10],[-10,10],[-10,-10],[10,-10]]
@@ -39,8 +67,8 @@ const Tv = ({speed}) => {
         let i = setInterval(()=>{
             setCoordinate(([x,y])=>{
                 
-                let left = x+dirL.current*1/speed;
-                let top = y+dirT.current*1/speed;
+                let left = x+dirL.current*dL.current;
+                let top = y+dirT.current*dR.current;
                 if((left <= 0 && top <= 0)||(left<=0 && top >= tvEl.clientHeight-el.offsetHeight) 
                   || (left >= tvEl.clientWidth-el.offsetWidth && top <= 0) 
                   || (left >= tvEl.clientWidth-el.offsetWidth && top>=tvEl.clientHeight-el.offsetHeight)){
@@ -50,12 +78,18 @@ const Tv = ({speed}) => {
                     location.reload()
                   }
                 if(left<0 || left>tvEl.clientWidth-el.offsetWidth){
+                  if(left<0) left = 0
+                  if(left>tvEl.clientWidth-el.offsetWidth) left = tvEl.clientWidth-el.offsetWidth
                   dirL.current*=-1
-                  left = x+dirL;
+                  // left = x+dirL;
+                  dL.current = randomSpeed();
                 }
                 if(top<0 || top>tvEl.clientHeight-el.offsetHeight){
+                  if(top<0) top = 0
+                  if(top>tvEl.clientHeight-el.offsetHeight) top = tvEl.clientHeight-el.offsetHeight
                   dirT.current*=-1
-                  top = y+dirT;
+                  // top = y+dirT;
+                  dR.current = randomSpeed();
                 }
                 
                 
@@ -64,7 +98,8 @@ const Tv = ({speed}) => {
                 // console.log(speed)
                 return [left,top];
             });
-        },16);
+        },24/speed);
+        // },0.001);
         console.log(speed)
         return ()=> clearInterval(i)
     },[speed])
